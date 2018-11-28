7297c95e25883288b34070194fa2337c1082bc5a
Gregor zavadskiy <gregor.zavadskiy@coidea.agency>
2018-11-28 12:10:55 +0200

    first commit

---
+++ Smack/Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by piatkovskyi on 11/28/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
