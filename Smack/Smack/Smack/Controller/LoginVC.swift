927fae0b16aebe5973e1fa0c71254fd7f9ba8298
Gregor zavadskiy <gregor.zavadskiy@coidea.agency>
2018-11-28 14:26:41 +0200

    Created LoginVC UI

---
+++ Smack/Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by piatkovskyi on 11/28/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by piatkovskyi on 11/28/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
