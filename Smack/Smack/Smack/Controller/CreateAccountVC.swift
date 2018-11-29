b2bbe7637fa2d4bcd75a0326e02585bb1979954f
Gregor zavadskiy <gregor.zavadskiy@coidea.agency>
2018-11-29 13:22:20 +0200

    Added registration method realisation and roundButton class

--- Smack/Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Smack/Controller/CreateAccountVC.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
     
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passwordTxt.text , passwordTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
     }
     
     @IBAction func closePressed(_ sender: Any) {
---
+++ Smack/Smack/Smack/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by piatkovskyi on 11/29/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+}
